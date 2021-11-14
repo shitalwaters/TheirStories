ce3050b79b9df1b44e63efd97ac4e36023332c5d
Sabrina Wang <sabrinawang@Sabrinas-MBP.hsd1.ca.comcast.net>
2021-11-13 17:15:39 -0800

    Added mentor table in database

--- coolkats_app/admin.py
+++ coolkats_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Mentor
 # Register your models here.
+class MentorAdmin(admin.ModelAdmin):
+    list_display=('id','name', 'fields', 'motivations')
+
+admin.site.register(Mentor)
--- coolkats_app/models.py
+++ coolkats_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Mentor(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    street_address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
+    state = models.CharField(max_length=20, null=True)
+    description = models.TextField(blank=True)
+    story=models.TextField(blank=True)
+    web_link = models.CharField(max_length=200, null=True)
+    image = models.TextField(blank=True)
+    fields=models.CharField(max_length=350, null=True)
+    motivations=models.CharField(max_length=350, null=True) 
