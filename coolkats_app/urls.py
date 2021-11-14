a30cb28e0334bc55b1a1f490abe79281f7d02fb0
Sabrina Wang <sabrinawang@Sabrinas-MBP.hsd1.ca.comcast.net>
2021-11-13 21:27:40 -0800

    Add links to URL

--- coolkats_app/urls.py
+++ coolkats_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("signin/", views.signin, name="signin"),
     path("signup/", views.signup, name="signup"),
     path("logout/", views.logout, name="logout"),
+    path("resources/", views.resources, name="resources"),
     path("mentor/<int:mentor_id>", views.mentor, name="mentor"),
     path("booksession/<int:mentor_id>/<int:user_id>/<int:session_id>", views.bookSession, name="bookSession")
 ]
